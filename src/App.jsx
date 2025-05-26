77b6fc522b62062aecf016bdb3181024c9fa0201
Sneha Yadav <164773937+SNH078@users.noreply.github.com>
2025-05-26 17:46:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import { FaGithub, FaLinkedin, FaEnvelope, FaArrowUp } from 'react-icons/fa';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
@@ -14,6 +15,47 @@ function App() {
      <Route path='/' element={<Cards />} />
      <Route path='/cart/:id' element={<CardsDetails />} />
    </Routes>
+
+    <footer className="py-6 bg-gray-800 text-white text-center my-4">
+      <div className="flex justify-center flex-wrap  mb-4 ">
+
+        <a
+          href="https://github.com/SNH078/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center gap-3 text-white hover:text-cyan-300 transition text-base mx-1"
+
+        >
+          <FaGithub className="text-xl" />
+         
+        </a>
+        <a
+          href="https://www.linkedin.com/in/sneha-yadav-sy/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center gap-3 text-white hover:text-cyan-300 transition mx-1 text-base"
+        >
+          <FaLinkedin className="text-xl" />
+          
+        </a>
+        <a
+          href="mailto:you@example.com"
+          className="flex items-center gap-3 text-white hover:text-cyan-300 mx-1 transition text-base"
+        >
+          <FaEnvelope className="text-xl" />
+         
+        </a>
+        <a
+          href="#top"
+          className="flex items-center gap-3 text-white hover:text-cyan-300  mx-1 transition text-base"
+        >
+          <FaArrowUp className="text-xl" />
+          Back to top
+        </a>
+      </div>
+      <p className="text-gray-400 text-sm">© 2025 SNH078. All rights reserved.</p>
+    </footer>
+
   </>
   );
 }
